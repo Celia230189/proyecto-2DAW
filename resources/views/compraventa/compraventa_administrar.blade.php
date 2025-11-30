9f75d0746d6004909c7a537bf2c35505de240434
Celia230189 <rjn52081@educastur.es>
2025-11-30 12:18:28 +0100

    ..

--- resources/views/compraventa/compraventa_administrar.blade.php
+++ resources/views/compraventa/compraventa_administrar.blade.php
@@ -3,20 +3,20 @@
 @section('contenido_principal')
 <style>
     .imagen {
-            width: 50%;
-        }
+        width: 100%;
+        height: 150px;
+        object-fit: cover;
+    }
 
-        #imagen-td {
-            width: 20%;
-        }
+    #imagen-td {
+        width: 200px;
+    }
 
-        #opciones {
-            padding: 2%;
-            margin-left: 2%;
-        }
-</style>
-
-<div style="height: 20px;"></div>
+    #opciones {
+        padding: 2%;
+        margin-left: 2%;
+    }
+</style><div style="height: 20px;"></div>
 
     {{-- Botón para añadir un producto a la venta --}}
     <div id="opciones">
@@ -24,45 +24,25 @@
     </div>
 
     {{-- Tabla de listado de tus productos en venta --}}
-    <table class="table table-hover text-center">
-        <thead class="table-dark">
-            <tr>
-                <th scope="col">Imagen</th>
-                <th scope="col">Título</th>
-                <th scope="col">Precio</th>
-                <th scope="col">Acciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($datosCompraventa as $producto)
-                <tr>
-                    {{-- Columna Imagen --}}
-                    <td id="imagen-td">
+    <table class="table table-hover text-center">
+        <thead class="table-dark">
+            <tr>
+                <th scope="col">Imagen</th>
+                <th scope="col">Título</th>
+                <th scope="col">Precio</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($datosCompraventa as $producto)
+                <tr>
+                    {{-- Columna Imagen --}}
+                    <td id="imagen-td">
                         <img class="card-img-top imagen" src="{{asset($producto->imagen)}}" alt="..." />
                     </td>
                     {{-- Columna Título y Precio (datos del producto) --}}
-                    <td>{{$producto->nombre_producto}}</td>
-                    <td>{{$producto->precio}}</td>
-
-                    {{-- Columna Acciones (EDITAR y BORRAR) --}}
-                    <td>
-                        {{-- Usamos d-flex para apilar los botones verticalmente --}}
-                        <div class="d-flex flex-column align-items-center">
-                            {{-- Botón Editar (usa la ruta GET) --}}
-                            <a href="{{ route('editarCompraventa', $producto->id) }}" class="btn btn-primary btn-sm mb-2">
-                                Editar <i class="fa-solid fa-pen"></i>
-                            </a>
-
-                            {{-- Formulario Borrar (POST con token CSRF) --}}
-                            <form action="{{route('borrarCompraventa', $producto->id)}}" method="POST">
-                                @csrf {{-- Token de seguridad --}}
-                                <button class="btn btn-danger btn-sm">Borrar <i class="fa-solid fa-trash"></i></button>
-                            </form>
-                        </div>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-
-@endsection
+                    <td>{{$producto->nombre_producto}}</td>
+                    <td>{{$producto->precio}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>@endsection
